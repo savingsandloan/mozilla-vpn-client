67311dbd2099d72a01650de99d1eda2b88a8e11e
Sebastian Streich <streich.mobile@gmail.com>
2023-11-02 16:31:21 +0100

    WIP, Macos

--- extension/loophole/go.mod
+++ extension/loophole/go.mod
@@ -1,6 +1,6 @@
 module github.com/mozilla-mobile/mozilla-vpn-client/extension/loophole
 
-go 1.21.3
+go 1.21
 
 require (
 	go4.org/mem v0.0.0-20220726221520-4f986261bf13 // indirect
--- extension/loophole/natives_darwin.go
+++ extension/loophole/natives_darwin.go
@@ -1,7 +1,6 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-
 package main
 
 import (
@@ -9,10 +8,9 @@ import (
 )
 
 /**
-* Does not create a specific dial context on windows
-* the vpn client will use split tunneling to make sure the
-* dialer is using the "real" network device :)
-**/
+* TODO: Make sure the Dialer will create 
+* a socket using socksoptions.
+*/
 func getDialer() net.Dialer {
 	return net.Dialer{}
 }
